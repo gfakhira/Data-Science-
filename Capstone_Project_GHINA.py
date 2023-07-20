6de5044ac4485a54dee7c3f296e6333aa19022f7
gfakhira <88647145+gfakhira@users.noreply.github.com>
2023-07-20 22:01:08 +0700

    Add files via upload

--- Capstone_Project_GHINA.py
+++ Capstone_Project_GHINA.py
@@ -1,19 +1,20 @@
 import datetime
 
-List_layanan_service = [{"Nomor Member" : 2202185, "Nama" : "Tinky Winky", "Tanggal Pelaporan" : datetime.date(2022,1,5), "Status" : "OPEN"},
-                        {"Nomor Member" : 2202301, "Nama" : "Dipsy", "Tanggal Pelaporan" : datetime.date(2022,1,6), "Status" : "OPEN"},
-                        {"Nomor Member" : 2203033, "Nama" : "Lala", "Tanggal Pelaporan" : datetime.date(2022, 1, 12), "Status" : "OPEN"},
-                        {"Nomor Member" : 2203055, "Nama" : "Asep", "Tanggal Pelaporan" : datetime.date(2022,1,14), "Status" : "OPEN"},
-                        {"Nomor Member" : 2204055, "Nama" : "Tabi", "Tanggal Pelaporan" : datetime.date(2022,1,17), "Status" : "OPEN"}]
+List_layanan_service = [{"Nomor Member" : 2202185, "Nama" : "Tinky Winky", "Alamat": "Jl. Baru", "Tanggal Pelaporan" : datetime.date(2022,1,5), "Status" : "OPEN"},
+                        {"Nomor Member" : 2202301, "Nama" : "Dipsy", "Alamat": "Tanah Sareal", "Tanggal Pelaporan" : datetime.date(2022,1,6), "Status" : "OPEN"},
+                        {"Nomor Member" : 2203033, "Nama" : "Lala", "Alamat": "Komp. Yasmin", "Tanggal Pelaporan" : datetime.date(2022, 1, 12), "Status" : "OPEN"},
+                        {"Nomor Member" : 2203055, "Nama" : "Asep", "Alamat": "Cimanggu Permai", "Tanggal Pelaporan" : datetime.date(2022,1,14), "Status" : "OPEN"},
+                        {"Nomor Member" : 2204055, "Nama" : "Tabi", "Alamat": "Tanah Baru", "Tanggal Pelaporan" : datetime.date(2022,1,17), "Status" : "OPEN"}]
 
 # Menu 1 Menampilkan Daftar Customer
 def menudaftar():
     print("\nDaftar Customer: \n")
-    print("Nomor Member \t| Nama Customer \t| Tanggal Pelaporan \t| Status")
+    print("Nomor Member \t| Nama Customer \t| Alamat \t\t| Tanggal Pelaporan \t| Status")
     for a in range(len(List_layanan_service)) :
         for a in range(0, 5):
-            print('{} \t| {}  \t\t| {} \t\t| {}'.format(List_layanan_service[a]["Nomor Member"], 
-                                              List_layanan_service[a]["Nama"], 
+            print('{} \t| {}  \t\t| {} \t\t| {} \t\t| {}'.format(List_layanan_service[a]["Nomor Member"], 
+                                              List_layanan_service[a]["Nama"],
+                                              List_layanan_service[a]["Alamat"], 
                                               List_layanan_service[a]["Tanggal Pelaporan"],
                                               List_layanan_service[a]["Status"]))
         return menudaftar
@@ -24,18 +25,21 @@ def menuregis():
     while True:
         regisnomember = input("\nRegistrasi \nInput Nomor Member: \t")
         regisnama = input("Input Nama Customer: \t\t")
+        regisalamat = input("Input Alamat: \t")
         registanggal = datetime.date.today()
         regisstatus = "OPEN"
 
         List_layanan_service.append({ "Nomor Member" : regisnomember, 
-                                     "Nama" : regisnama, 
+                                     "Nama" : regisnama,
+                                     "Alamat": regisalamat, 
                                      "Tanggal Pelaporan": registanggal, 
                                      "Status": regisstatus})
 
-        print('{} \t| {} \t| {} \t| {}'.format("Nomor Member", "Nama Customer", "Tanggal Pelaporan", "Status"))
+        print('{} \t| {} \t| {} \t| {} \t| {}'.format("Nomor Member", "Nama Customer", "Alamat", "Tanggal Pelaporan", "Status"))
         for a in range(len(List_layanan_service)) :
-            print('{} \t| {}  \t\t| {} \t\t| {}'.format(List_layanan_service[a]["Nomor Member"], 
-                                              List_layanan_service[a]["Nama"], 
+            print('{} \t| {}  \t\t| {} \t| {} \t\t| {}'.format(List_layanan_service[a]["Nomor Member"], 
+                                              List_layanan_service[a]["Nama"],
+                                              List_layanan_service[a]["Alamat"], 
                                               List_layanan_service[a]["Tanggal Pelaporan"],
                                               List_layanan_service[a]["Status"]))
             
@@ -49,9 +53,9 @@ def menuregis():
 # Menu 3; Menghapus Data Customer dengan Pembatalan
 def menubatal():
     print("\nStatus Pelayanan Service: \n")
-    print('{} \t| {} \t| {} \t| {} \t| {}'.format("Index", "Nomor Member", "Nama Customer", "Tanggal Pelaporan", "Status"))
+    print('{} \t| {} \t| {} \t| {} \t| {} \t| {}'.format("Index", "Nomor Member", "Nama Customer", "Alamat", "Tanggal Pelaporan", "Status"))
     for index, item in enumerate(List_layanan_service):
-        print(f"{index} \t| {item['Nomor Member']}  \t| {item['Nama']}  \t\t| {item['Tanggal Pelaporan']} \t\t| {item['Status']}")
+        print(f"{index} \t| {item['Nomor Member']}  \t| {item['Nama']}  \t\t| {item['Alamat']} \t| {item['Tanggal Pelaporan']} \t\t| {item['Status']}")
         
     hapus_data = int(input("\nMasukkan Index Data Customer Yang Ingin Dibatalkan: \t"))
 
@@ -60,12 +64,13 @@ def menubatal():
 
         List_layanan_service.pop(hapus_data)
 
-        print('{} \t| {} \t| {} \t| {}'.format("Nomor Member", "Nama Customer", "Tanggal Pelaporan", "Status"))
+        print('{} \t| {} \t| {} \t| {} \t| {}'.format("Nomor Member", "Nama Customer", "Alamat", "Tanggal Pelaporan", "Status"))
         for a in range(len(List_layanan_service)) :
-            print('{} \t| {}  \t\t| {} \t\t| {}'.format(List_layanan_service[a]["Nomor Member"], 
-                                                        List_layanan_service[a]["Nama"], 
-                                                        List_layanan_service[a]["Tanggal Pelaporan"],
-                                                        List_layanan_service[a]["Status"]))
+            print('{} \t| {}  \t\t| {} \t| {} \t\t| {}'.format(List_layanan_service[a]["Nomor Member"], 
+                                              List_layanan_service[a]["Nama"],
+                                              List_layanan_service[a]["Alamat"], 
+                                              List_layanan_service[a]["Tanggal Pelaporan"],
+                                              List_layanan_service[a]["Status"]))
         print("\nPelayanan Service Berhasil Dibatalkan")
 
     else:
@@ -75,16 +80,16 @@ def menubatal():
 # Menu 4; Menampilkan Jadwal Pelaksanaan Service 
 def menujadwal():              
     print("\nDaftar Pelayanan Service: ")
-    print('{} \t| {} \t| {} \t| {} \t| {}'.format("Index", "Nomor Member", "Nama Customer", "Tanggal Pelaporan", "Status"))
+    print('{} \t| {} \t| {} \t| {} \t| {} \t| {}'.format("Index", "Nomor Member", "Nama Customer", "Alamat","Tanggal Pelaporan", "Status"))
     for index, item in enumerate(List_layanan_service):
-        print(f"{index} \t| {item['Nomor Member']}  \t| {item['Nama']}  \t\t| {item['Tanggal Pelaporan']} \t\t| {item['Status']}")
+        print(f"{index} \t| {item['Nomor Member']}  \t| {item['Nama']}  \t\t| {item['Alamat']} \t| {item['Tanggal Pelaporan']} \t\t| {item['Status']}")
     
 
     print("\nPenjadwalan Pelayanan Service: ")
-    print('{} \t| {} \t| {} \t| {} \t| {}'.format("Index", "Nomor Member", "Nama Customer", "Tanggal Service", "Status"))
+    print('{} \t| {} \t| {} \t| {} \t| {} \t| {}'.format("Index", "Nomor Member", "Nama Customer", "Alamat", "Tanggal Service", "Status"))
     for index, item in enumerate(List_layanan_service):
         tanggal = item["Tanggal Pelaporan"]+ datetime.timedelta(days=2)
-        print(f"{index} \t| {item['Nomor Member']}  \t| {item['Nama']}  \t\t| {tanggal} \t\t| {item['Status']}")
+        print(f"{index} \t| {item['Nomor Member']}  \t| {item['Nama']}  \t\t| {item['Alamat']} \t| {tanggal} \t\t| {item['Status']}")
 
 def exit():
     print("\nTerima Kasih dan Sampai Jumpa!")
